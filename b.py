367005cde839eeda3c511278187fb049c833f45a
kWAY TV <mvnieto2127@gmail.com>
2022-06-14 20:57:08 +0200

    Update b.py

--- b.py
+++ b.py
@@ -42,21 +42,12 @@ intro = f"""
 ||__|||__|||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/_______\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
 {Fore.MAGENTA}═════════════════════════════════════════════════════════════════════════════════════{Fore.RESET}
-
 """
-
-def rsCount():
-    sCount = 0
 
 sCount = 0
 
-class Counter():
-    def __init__(self):
-        self.sCount = 1
-
-    def count(self):
-        os.system(f"title Shillify Discord - Working... - {self.sCount + 1} message(s) sent this session.")
-        self.sCount += 1
+def rsCount():
+    sCount = 0
 
 
 logs = f""" 
@@ -74,15 +65,18 @@ with open('config.json', "rb") as infile:
 with open('config.json', "rb") as infile:
     temp_config = json.load(infile)
 
-os.system(f"title Shillify Discord - Starting...")
-counter_obj = Counter()
+os.system(f"title Shillify Discord - Starting... - discord.gg/kws")
 
 if token == "null":
     clear()
     unset = True
     while unset == True:
-        slow_type(Fore.RED + "Error: " + Style.RESET_ALL + "No settings found!", 0.01)
+        slow_type(Fore.RED + "Error: " + Style.RESET_ALL + "No settings found! Creating config!", 0.01)
+        os.system(f"title Shillify Discord - Creating config. - discord.gg/kws")
+        time.sleep(1)
+        clear()
         slow_type(Fore.BLUE + "Input: " + Style.RESET_ALL + "Please enter your token: ", 0.01)
+        os.system(f"title Shillify Discord - Input your token...  - discord.gg/kws")
         token = input()
         url = "https://canary.discord.com/api/v9/users/@me"
 
@@ -124,12 +118,12 @@ async def on_ready():
     slow_type(intro + Style.RESET_ALL, 0.001)
     slow_type('Logged in as ' + Fore.GREEN + f'{client.user.name}#{client.user.discriminator}' + Style.RESET_ALL, 0.001)
     slow_type(logs + Style.RESET_ALL, 0.001)
-    os.system(f"title Shillify Discord - Ready!")
+    os.system(f"title Shillify Discord - Ready! - discord.gg/kws")
     time.sleep(1)
+    rsCount()
     advertise.start()
 
 decodedmsg = base64.b64decode(config["userdata"].get('message'))
-
 
 @tasks.loop(seconds=int(seconds))
 async def advertise():
@@ -141,7 +135,7 @@ async def advertise():
 
         if "null" in channels_config or len(channels_config) == 0:
 
-            if advertise.seconds == 0:
+            if advertise.seconds is 0:
                 seconds = int(config["userdata"].get('seconds'))
                 advertise.change_interval(seconds=seconds)
                 return
@@ -167,32 +161,14 @@ async def advertise():
                 temp_channels_config[id_e] = delay_e - advertise.seconds
 
         counter = 0
+
         for channel_id in channels_to_send:
-            try:
-                await client.get_channel(int(channel_id)).send(str(decodedmsg)[2:-1])
-                slow_type(
-                    Fore.GREEN + f"Success: " + Style.RESET_ALL + f" Successfully sent advertisment to channel id '{channel_id}'!", 0.01)
-                counter += 1
+            await client.get_channel(int(channel_id)).send(str(decodedmsg)[2:-1])
+            slow_type(
+                Fore.GREEN + f"Success: " + Style.RESET_ALL + f" Successfully sent advertisment to channel id '{channel_id}'!", 0.01)
+            counter += 1
 
-                counter_obj.count()
-
-            except:
-                slow_type(
-                    Fore.RED + "Error: " + Style.RESET_ALL + f" Could not send to channel id '{channel_id}', removing from list...",
-                    0.01)
-                del temp_config['userdata']['channels'][channel_id]
-                del config['userdata']['channels'][channel_id]
-                with open('config.json', "w") as jsfile:
-                    json.dump(config, jsfile)
-                    jsfile.close()
-
-                if len(temp_config['userdata']['channels']) == 0:
-                    seconds = config["userdata"].get('seconds')
-                    advertise.change_interval(seconds=seconds)
-                    slow_type(
-                        Fore.YELLOW + "Sleep: " + Style.RESET_ALL + f" Finished task, sleeping for {seconds} second(s).",
-                        0.02)
-                    return
+            os.system(f"title Shillify Discord - Working... - {counter} message(s) sent this session. - discord.gg/kws")
 
         seconds = 999999999999999
 
@@ -204,24 +180,23 @@ async def advertise():
             if delay_e < seconds:
                 seconds = delay_e
 
-        if advertise.seconds != 0:
-            slow_type(Fore.YELLOW + "Sleep: " + Style.RESET_ALL + f" Finished task, sleeping for {seconds} second(s).",
-                      0.02)
-
         advertise.change_interval(seconds=seconds)
 
 @client.command()
 async def h(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Loading help... - discord.gg/kws")
     help_all = slow_type(Fore.BLUE + "\nHelp: " + Style.RESET_ALL + f" Shillify discord help commands ", 0.01)
     slow_type(
-        f"\n {Fore.YELLOW}.add <channelid>{Fore.RESET} Add a channel to advertise.\n {Fore.YELLOW}.rem <channelid>{Fore.RESET} Remove a channel from the list.\n {Fore.YELLOW}.cm <message>{Fore.RESET} Set/change the message to be sent and restart.\n {Fore.YELLOW}.ct <newtoken>{Fore.RESET} Change actual token and restart.\n {Fore.YELLOW}.cd <newdelay>{Fore.RESET} Change actual delay and restart.\n {Fore.YELLOW}.rt{Fore.RESET} Delete the actual config and choose what to do in terminal.\n {Fore.YELLOW}.s{Fore.RESET} Shows the actual settings/config.\n {Fore.YELLOW}.r{Fore.RESET} Restart the tool.\n {Fore.YELLOW}.dc{Fore.RESET} Discord.",
+        f"\n {Fore.YELLOW}.add <channelID> <delay/empty for default>{Fore.RESET} Add a channel to advertise.\n {Fore.YELLOW}.rem <channelID>{Fore.RESET} Remove a channel from the list.\n {Fore.YELLOW}.cm <message>{Fore.RESET} Set/change the message to be sent and restart.\n {Fore.YELLOW}.ct <newtoken>{Fore.RESET} Change actual token and restart.\n {Fore.YELLOW}.cd <newdelay>{Fore.RESET} Change actual delay and restart.\n {Fore.YELLOW}.rt{Fore.RESET} Delete the actual config and choose what to do in terminal.\n {Fore.YELLOW}.s{Fore.RESET} Shows the actual settings/config.\n {Fore.YELLOW}.r{Fore.RESET} Restart the tool.\n {Fore.YELLOW}.dc{Fore.RESET} Discord.",
         0.01)
+    os.system(f"title Shillify Discord - Help loaded! - discord.gg/kws")
 
 
 @client.command()
 async def rem(ctx, *, id):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Removing channel... - discord.gg/kws")
     with open('config.json', "rb") as infile:
         config = json.load(infile)
     oldtokenvalue = config["userdata"].get('token')
@@ -236,11 +211,13 @@ async def rem(ctx, *, id):
         json.dump(config, jsfile)
         jsfile.close()
     slow_type(Fore.GREEN + "Success: " + Style.RESET_ALL + "Removed channel!", 0.01)
+    os.system(f"title Shillify Discord - Channel removed! - discord.gg/kws")
 
 
 @client.command()
 async def add(ctx, id, delay):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Adding channel... - discord.gg/kws")
     with open('config.json', "rb") as infile:
         config = json.load(infile)
     oldtokenvalue = config["userdata"].get('token')
@@ -272,11 +249,13 @@ async def add(ctx, id, delay):
             json.dump(config, jsfile)
             jsfile.close()
     slow_type(Fore.GREEN + "Success: " + Style.RESET_ALL + "Added channel!", 0.01)
+    os.system(f"title Shillify Discord - Added channel! - discord.gg/kws")
 
 
 @client.command()
 async def cm(ctx, *, msg):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Changing message... - discord.gg/kws")
     encodedmsg = str(base64.b64encode(bytes(msg, 'utf-8')))[2:-1]
     with open('config.json', "rb") as infile:
         config = json.load(infile)
@@ -294,11 +273,13 @@ async def cm(ctx, *, msg):
               0.01)
     time.sleep(2)
     subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
+    os.system(f"title Shillify Discord - Message changed! Restarting... - discord.gg/kws")
 
 
 @client.command()
 async def cd(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Changing delay... - discord.gg/kws")
     slow_type(Fore.BLUE + "Input: " + Style.RESET_ALL + "Enter new delay in seconds: ", 0.01)
     newdelay = int(input())
     with open('config.json', "rb") as infile:
@@ -318,11 +299,13 @@ async def cd(ctx):
               0.01)
     time.sleep(2)
     subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
+    os.system(f"title Shillify Discord - Delay changed! Restarting... - discord.gg/kws")
 
 
 @client.command()
 async def ct(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Changing token... - discord.gg/kws")
     slow_type(Fore.BLUE + "Input: " + Style.RESET_ALL + "Enter new token: ", 0.01)
     newtoken = input()
     with open('config.json', "rb") as infile:
@@ -342,11 +325,13 @@ async def ct(ctx):
               0.01)
     time.sleep(2)
     subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
+    os.system(f"title Shillify Discord - Token changed! Restarting... - discord.gg/kws")
 
 
 @client.command()
 async def rt(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Resetting config... - discord.gg/kws")
     with open('config.json', "rb") as infile:
         config = json.load(infile)
         update = {"userdata": {"token": "null", "seconds": "null", "message": "null", "channels": {"null": "null"}}}
@@ -363,19 +348,23 @@ async def rt(ctx):
         subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
     else:
         pass
+    os.system(f"title Shillify Discord - Config reseted! - discord.gg/kws")
 
 
 @client.command()
 async def r(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Restarting... - discord.gg/kws")
     slow_type(Fore.CYAN + "Restart: " + Style.RESET_ALL + "Trying to restart task!", 0.01)
     time.sleep(2)
     subprocess.call([sys.executable, os.path.realpath(__file__)] + sys.argv[1:])
+    os.system(f"title Shillify Discord - Restarted! - discord.gg/kws")
 
 
 @client.command()
 async def s(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Loading settings command... - discord.gg/kws")
     with open('config.json', "rb") as infile:
         sConfig = json.load(infile)
     sToken = config["userdata"].get('token')
@@ -395,14 +384,16 @@ async def s(ctx):
     slow_type(
         Fore.BLUE + "\nSettings:\n " + Style.RESET_ALL + f"Token: {sToken}\n Delay: {sHours}\n Channels: {sChannelids}\n Message: {sMsg}\n",
         0.01)
+    os.system(f"title Shillify Discord - Settings command loaded! - discord.gg/kws")
 
 
 @client.command()
 async def dc(ctx):
     await ctx.message.delete()
+    os.system(f"title Shillify Discord - Opening website... - discord.gg/kws")
     webbrowser.open('http://discord.gg/kws')
     slow_type(Fore.MAGENTA + "Success: " + Style.RESET_ALL + "Opening discord.gg/kws in browser!", 0.01)
-
+    os.system(f"title Shillify Discord - Website opened! - discord.gg/kws")
 
 client.run(token, bot=False)
 input()
